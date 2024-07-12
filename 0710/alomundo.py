449045053d8e530b082d215bb4c25a7bb591083e
pedromchd <48498905+pedromchd@users.noreply.github.com>
2024-07-12 08:45:30 -0300

    Alô mundo!

---
+++ 0710/alomundo.py
@@ -0,0 +1,12 @@
+print("Alô mundo dos Algoritmos!")
+# Essa linha o python não vai ler. É só para humanos
+# print("Eu amo o Prisco \n" 100)
+E = 7 # Minha primeira variável
+print(type(E))
+print(E)
+E = "e agora????"
+print(type(E))
+print(E)
+nome = input("Digite seu nome: ")
+print("Konnichiwa, " + nome)
+
